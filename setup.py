d6d16b13cb5e0a56657614ec28ea8a81025515fe
Arthur Pitzer <arthurpitzer@id.uff.br>
2019-11-09 18:48:15 -0300

    setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='gloop-lib',
+    packages=['gloop', 'gloop.channels'],
+    url='https://github.com/pitzer42/gloop-lib',
+    license='MIT',
+    author='Arthur Pitzer',
+)
